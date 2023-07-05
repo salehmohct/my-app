2bac528511b5cc1889c4a394e3ab7b53abbaf678
salehmohct <salehalict94@gmail.com>
2023-07-05 10:51:47 +0300

    finish the product context

--- src/Context/productContext.js
+++ src/Context/productContext.js
@@ -1,11 +1,16 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 import { Products } from "../Mook/Product";
 
-export const productContext = createContext(null);
+const productContext = createContext(null);
 const ProductProvider = ({ children }) => {
   return (
-    <productContext.Provider value={Products}>{children}</productContext.Provider>
+    <productContext.Provider value={Products}>
+      {children}
+    </productContext.Provider>
   );
 };
 
 export default ProductProvider;
+export const useProductContext = () => {
+  return useContext(productContext);
+};
